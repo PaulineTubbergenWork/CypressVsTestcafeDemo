92b9d4f5866149f79d66985f8757d27a4f256f9c
Pauline Tubbergen <98538764+PaulineTubbergenOrdina@users.noreply.github.com>
2023-01-31 15:32:05 +0100

    cypress een pageobject gegeven

--- Cypress/cypress/e2e/example.cy.js
+++ Cypress/cypress/e2e/example.cy.js
@@ -1,18 +1,20 @@
+import { urls, welkomsttekst, zoekveld, titeltekst } from "../page_objects/wikipedia.po"
+
 describe('Wikipedia', () => {
   it('checks that I can visit wikipedia', () => {
-    cy.visit('https://www.wikipedia.org')
+    cy.visit(urls.basicUrl)
   })
 
   it('Checks that I can find the Platypus page on the English Wikipedia', () => {
     // go to the English Wikipedia page
-    cy.visit('https://en.wikipedia.org/wiki/Main_Page')
+    cy.visit(urls.englishUrl)
     // check that the page is in English
-    cy.get('span.mw-headline').contains('Welcome to')
+    cy.get(welkomsttekst).contains('Welcome to')
     // search for the Platypus
-    cy.get('#searchInput[aria-label="Search Wikipedia"]').type('Platypus')
+    cy.get(zoekveld).type('Platypus')
     cy.contains('button', 'Search').click()
     // check that the Platypus page was found
-    cy.get('h1.firstHeading > span').should('be.visible')
-    cy.get('h1.firstHeading > span').should('have.text', 'Platypus')
+    cy.get(titeltekst).should('be.visible')
+    cy.get(titeltekst).should('have.text', 'Platypus')
   })
 })
---
+++ Cypress/cypress/page_objects/wikipedia.po.js
@@ -0,0 +1,8 @@
+export const urls = {
+    basicUrl: 'https://www.wikipedia.org',
+    englishUrl: 'https://en.wikipedia.org/wiki/Main_Page'
+}
+
+export const zoekveld = '#searchInput[aria-label="Search Wikipedia"]'
+export const welkomsttekst = 'span.mw-headline'
+export const titeltekst = 'h1.firstHeading > span'
